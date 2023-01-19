210411b45f2fb874124117ecad670716e18603d7
lalitha2407 <lalitha@bootlabstech.com>
2023-01-19 13:41:47 +0530

    commented app service block

--- main.tf
+++ main.tf
@@ -6,22 +6,22 @@ resource "azurerm_storage_account" "storage_account" {
   account_replication_type = var.account_replication_type
 }
 
-resource "azurerm_app_service_plan" "appservice" {
-  name                = var.appservice_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+# resource "azurerm_app_service_plan" "appservice" {
+#   name                = var.appservice_name
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
 
-  sku {
-    tier = var.sku_tier
-    size = var.sku_size
-  }
-}
+#   sku {
+#     tier = var.sku_tier
+#     size = var.sku_size
+#   }
+# }
 
 resource "azurerm_logic_app_standard" "logic_app" {
   name                       = var.logic_app_name
   location                   = var.location
   resource_group_name        = var.resource_group_name
-  app_service_plan_id        = azurerm_app_service_plan.appservice.id
+  # app_service_plan_id        = azurerm_app_service_plan.appservice.id
   storage_account_name       = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
 }
